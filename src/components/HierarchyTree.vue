202d15e8546baaa6d376996cd599b0dce0093e6c
Kleine <guido.kleine@capgemini.com>
2024-09-16 07:44:57 +0200

    Started the project

---
+++ src/components/HierarchyTree.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="grape">
+    </div>
+</template>
+
+<script>
+import * as d3 from "d3";
+
+export default {
+    props: ['data'],
+    setup(props) {
+        console.log(props.data)
+    },
+    mounted() {
+        const width = 960;
+        const height = 500;
+
+        d3.select('#grape')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
+    }
+}
+</script>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <h1 id="homePageTitle">Hierarchy display</h1>
+        <hierarchy-tree :data="data" ></hierarchy-tree>
+    </div>
+</template>
+
+<script>
+import HierarchyTree from './HierarchyTree.vue';
+
+export default {
+  name: 'HomePage',
+  components: { HierarchyTree },
+  data() {
+    return {
+        "data":[
+            {   
+                "name":"A",
+                "description":"This is a description of A", 
+                "parent":""
+            },
+            {
+                "name":"B",
+                "description":"This is a description of B", 
+                "parent":"A"
+            },
+            {
+                "name":"C",
+                "description":"This is a description of C", 
+                "parent":"A"
+            },
+            {
+                "name":"D",
+                "description":"This is a description of D", 
+                "parent":"A"
+            },
+            {
+                "name":"B-1",
+                "description":"This is a description of B-1", 
+                "parent":"B"
+            },
+            {
+                "name":"B-2",
+                "description":"This is a description of B-2", 
+                "parent":"B"
+            },
+            {
+                "name":"B-3",
+                "description":"This is a description of B-3", 
+                "parent":"B"
+            }
+        ]
+    }}
+}
+</script>
